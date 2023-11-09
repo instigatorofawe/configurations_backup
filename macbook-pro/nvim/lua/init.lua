09cf6a4e8d7548b6edb99b704c486201b12314aa
Ran Liu <xabsox@gmail.com>
2023-11-09 13:02:05 +0800

    Update initialization of nvim plugins

--- macbook-pro/nvim/lua/init.lua
+++ macbook-pro/nvim/lua/init.lua
@@ -1,7 +1,7 @@
 require('mason').setup{}
 require('mason-lspconfig').setup{
     ensure_installed = {
-        "lua_ls", "marksman", "bashls"
+        "lua_ls", "marksman", "bashls", "texlab"
     }
 }
 
@@ -29,3 +29,4 @@ end
 require'lspconfig'.marksman.setup{}
 require'lspconfig'.lua_ls.setup{}
 require'lspconfig'.bashls.setup{}
+require'lspconfig'.texlab.setup{}
