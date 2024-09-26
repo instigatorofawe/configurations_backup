7704faafeee07e72a54e9d84f3a5a217301c0836
Ran Liu <xabsox@gmail.com>
2024-09-26 14:34:15 -0700

    Update tmux and nvim configurations on macbook

--- macbook-pro/nvim/lua/init.lua
+++ macbook-pro/nvim/lua/init.lua
@@ -1,7 +1,7 @@
 require('mason').setup{}
 require('mason-lspconfig').setup{
     ensure_installed = {
-        "lua_ls", "marksman", "bashls", "texlab"
+        "lua_ls", "marksman", "bashls", "texlab", "rust_analyzer"
     }
 }
 
@@ -18,15 +18,23 @@ if vim.fn.executable('pyright') == 1 then
     require'lspconfig'.pyright.setup{}
 end
 
-if vim.fn.executable('R') == 1 then
-    require'lspconfig'.r_language_server.setup{}
-end
+-- if vim.fn.executable('R') == 1 then
+--     require'lspconfig'.r_language_server.setup{}
+-- end
 
-if vim.fn.executable('sourcekit-lsp') == 1 then
-    require'lspconfig'.sourcekit.setup{}
-end
+-- if vim.fn.executable('sourcekit-lsp') == 1 then
+--     require'lspconfig'.sourcekit.setup{}
+-- end
 
 require'lspconfig'.marksman.setup{}
 require'lspconfig'.lua_ls.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.texlab.setup{}
+require'lspconfig'.rust_analyzer.setup{
+
+    on_attach = function(client, bufnr)
+        vim.lsp.inlay_hint.enable(true, {bufnr})
+    end
+    
+}
+
