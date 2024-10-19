370a76c3931ff3244262e4e62e3d5f5b19f9b288
Ran Liu <xabsox@gmail.com>
2024-10-19 11:55:56 -0700

    Don't ensure tree-sitter installs

--- nvim/init.lua
+++ nvim/init.lua
@@ -113,7 +113,7 @@ require("lazy").setup({
 				local configs = require("nvim-treesitter.configs")
 
 				configs.setup({
-					ensure_installed = "all",
+					-- ensure_installed = "all",
 					sync_install = false,
 					highlight = { enable = true },
 					indent = { enable = true },
