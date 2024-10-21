775b5fe65c889bd932c7f2828ecc23b54d68aeeb
Ran Liu <xabsox@gmail.com>
2024-10-21 10:54:13 -0700

    Signature cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,6 +74,7 @@ require("lazy").setup({
 				"hrsh7th/cmp-buffer",
 				"hrsh7th/cmp-path",
 				"hrsh7th/cmp-cmdline",
+				"hrsh7th/cmp-nvim-lsp-signature-help",
 			},
 		},
 		{
@@ -189,6 +190,8 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 	}, {
 		{ name = "buffer" },
+	}, {
+		{ name = "nvim_lsp_signature_help" },
 	}),
 })
 
