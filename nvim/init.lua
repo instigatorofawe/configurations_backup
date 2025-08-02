867bfaa3a5b52b6e32499c8facf2b57dfb10e5db
Ran Liu <ran.liu.2021@gmail.com>
2025-08-02 10:31:58 -0700

    Update nvim configuration for XCode

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,6 +200,18 @@ require("lazy").setup({
 			end,
 		},
 		{
+			"wojciech-kulik/xcodebuild.nvim",
+			dependencies = {
+				"nvim-telescope/telescope.nvim",
+				"MunifTanjim/nui.nvim",
+				"folke/snacks.nvim", -- (optional) to show previews
+				"nvim-tree/nvim-tree.lua", -- (optional) to manage project files
+				"stevearc/oil.nvim", -- (optional) to manage project files
+				"nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+			},
+            opts = {}
+		},
+		{
 			"hrsh7th/nvim-cmp",
 			-- event = "InsertEnter",
 			dependencies = {
